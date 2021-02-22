9375cab7e27426c54c170b3db75eec94f8796a15
James Fuller <68349438+kux9@users.noreply.github.com>
2021-02-22 14:44:10 -0500

    Add files via upload

---
+++ Rfunctions/get_riskmatrix_v3_KM.R
@@ -0,0 +1,84 @@
+function(typex){
+
+  dir.root<- ifelse(dir.exists(paste0("C:/Users/",Sys.getenv("USERNAME"),"/CDC/International Task Force-COVID19 - DataViz/Data and Analysis/")),
+                    paste0("C:/Users/",Sys.getenv("USERNAME"),"/CDC/International Task Force-COVID19 - DataViz/Data and Analysis/"),
+                    ifelse(dir.exists(paste0("C:/Users/",Sys.getenv("USERNAME"),"/CDC/ITF-COVID19 International Task Force - DataViz/Data and Analysis/")),
+                           paste0("C:/Users/",Sys.getenv("USERNAME"),"/CDC/ITF-COVID19 International Task Force - DataViz/Data and Analysis/"),
+                           "Directory does not exist"))
+# Function to load
+ldpkg <- function(x){
+  for( i in x ){
+    #  require returns TRUE invisibly if it was able to load package
+    if( ! require( i , character.only = TRUE ) ){
+      #  If package was not able to be loaded then re-install
+      install.packages( i , dependencies = TRUE )
+      #  Load package after installing
+      require( i , character.only = TRUE )
+    }
+  }
+}
+
+# folder where all R functions are housed
+rfunctions.dir <- paste0(dir.root, "PowerBI/R_scripts_testing/r_functions/")
+
+# function to get the base JHU data with cases and deaths daily/cumulative 
+fun_ncov <- dget(paste0(rfunctions.dir, "get_ncov_data.R"))
+# fun_rt   <- dget(paste0(rfunctions.dir, "Rt_ncov.R"))
+
+# Load/install packages 
+ldpkg(c("tidyverse"))
+
+
+# Getting the datasets
+ncov <- fun_ncov()  # The cases and deaths
+# rt  <- fun_rt()     # Rt values (7-day sliding window)
+
+df <- ncov %>% select(-ou_date_match)
+
+df1 <- df %>% 
+  mutate(cases_cum = if_else(is.na(`Cumulative Cases`), 0, `Cumulative Cases`)) %>% 
+  mutate(deaths_cum = if_else(is.na(`Cumulative Deaths`), 0, `Cumulative Deaths`)) %>% 
+  mutate_if(is.numeric, ~replace(., .<0, 0)) %>% 
+  group_by(data_source, country_code) %>%
+  arrange(Date) %>% 
+  mutate(wkcase =  cases_cum - lag(cases_cum, 7)) %>% 
+  mutate(prev_wkcase =  lag(cases_cum, 7)-lag(cases_cum, 14)) %>% 
+  mutate(wkdeath =  deaths_cum - lag(deaths_cum, 7)) %>% 
+  mutate(prev_wkdeath =  lag(deaths_cum, 7)-lag(deaths_cum, 14)) %>% 
+  ungroup() %>% 
+  mutate_if(is.numeric, ~replace(., .<0, NA_real_)) %>% 
+  mutate(case_diff = wkcase-prev_wkcase) %>%
+  mutate(death_diff = wkdeath-prev_wkdeath) %>% 
+  mutate(wkcase_change = if_else(prev_wkcase>0,
+                                 (case_diff)/prev_wkcase,
+                                 NA_real_)) %>%
+  mutate(wkdeath_change = if_else(prev_wkdeath>0,
+                                  (death_diff)/prev_wkdeath,
+                                  NA_real_)) %>%
+  mutate(inci = if_else(`Population 2020`>0, 
+                        ((wkcase/`Population 2020`)/7)*100000, 
+                        NA_real_)) %>% 
+  mutate(incideath = if_else(`Population 2020`>0, 
+                             ((wkdeath/`Population 2020`)/7)*100000, 
+                             NA_real_)) %>% 
+  mutate(pct_chng =if_else( !(is.na(wkcase_change)), wkcase_change*100, NA_real_)) %>%
+  mutate(pct_chngdeath =if_else( !(is.na(wkdeath_change)), wkdeath_change*100, NA_real_)) %>%
+  mutate(Rt = 1) %>%
+  mutate(ou_src_match = paste(country_code, data_source, sep="_"))
+
+crossx <- df1 %>% select(-ou_date_src_match) %>% 
+  group_by(data_source) %>% 
+  mutate(datex = max(Date)) %>% 
+  ungroup() %>% 
+  filter(Date == datex) %>% 
+  select(-datex)
+
+if (typex == "date"){
+  return(df1)} else if (typex == "cross"){
+    return(crossx)}
+}
+
+
+
+
+
